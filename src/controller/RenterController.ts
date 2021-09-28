2c146aba1590d6d765a1e74423141589fa07fddc
baogiadinh1003 <baodpgpd03125@gmail.com>
2021-09-28 14:51:34 +0700

    sample crud for renter

--- src/controller/RenterController.ts
+++ src/controller/RenterController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 import mongoose from "mongoose";
 import { Renter } from '../model/Renter';
+import {validateDate, validateAccountStatus, validatePhone, validateEmail} from '../util/validation';
 
 /**
  * Login in using username and password.
@@ -16,6 +17,9 @@ export const postLogin = async (req: Request, res: Response) => {
  * @route POST /login
  */
 export const postRegister = async (req: Request, res: Response) => {
+    if (!validateDate(req.body.renterDateRegister) || !validateAccountStatus(req.body.accountStatus) || !validatePhone(req.body.renterPhone) || !validateEmail(req.body.renterEmail)) {
+        return res.status(400).send("Validation fail");
+    }
     let renter = new Renter(req.body);
     try { 
         let result = await renter.save();   
@@ -23,4 +27,60 @@ export const postRegister = async (req: Request, res: Response) => {
     } catch (error) {
         return res.status(500).send("Sign in fail");   
     };
+};
+
+/**
+ * Get all renter
+ * 
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+export const getAllRenter = async (req: Request, res: Response) => {
+    let renters = await Renter.find({});
+    return res.status(200).send(renters);
+};
+
+/**
+ * function update renter
+ * 
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+export const postUpdateRenter = async (req: Request, res: Response)  => {
+    if (!validateDate(req.body.renterDateRegister) || !validateAccountStatus(req.body.accountStatus) || !validatePhone(req.body.renterPhone) || !validateEmail(req.body.renterEmail)) {
+        return res.status(400).send("Validation fail");
+    }
+    try {
+        let renter = await Renter.findByIdAndUpdate(req.body._id, req.body, {new:false});
+        if (renter === null || renter === undefined) {
+            return res.status(200).send("1");
+        }
+        return res.status(200).send("0");
+    } catch (error) {
+        console.log(error);
+        
+        return res.sendStatus(500).send("Update error");
+    }
+};
+
+/**
+ * function delete renter
+ * 
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+export const postDeleteRenter = (req: Request, res: Response)  => {
+    try {
+        Renter.findByIdAndDelete(req.body._id, (err: Error, res: any) => {
+            if (err) {
+                return res.status(200).send("1");
+            }
+        });
+        return res.status(200).send("0");
+    } catch (error) {
+        return res.sendStatus(500).send("Update error");
+    }
 };
--- src/routers/renterRouters.ts
+++ src/routers/renterRouters.ts
@@ -9,3 +9,15 @@ export const renterLogin = (req: express.Request, res: express.Response) => {
 export const renterRegister = (req: express.Request, res: express.Response) => {
     return renterController.postRegister(req,res);
 };
+
+export const renterList = (req: express.Request, res: express.Response) => {
+    return renterController.getAllRenter(req,res);
+};
+
+export const renterUpdate = (req: express.Request, res: express.Response) => {
+    return renterController.postUpdateRenter(req,res);
+};
+
+export const renterDelete = (req: express.Request, res: express.Response) => {
+    return renterController.postDeleteRenter(req,res);
+};
