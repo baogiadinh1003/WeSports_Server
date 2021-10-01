48b001ea7ec2ed796e207c6c97ec54b34e86a6ca
baogiadinh1003 <baodpgpd03125@gmail.com>
2021-10-02 04:25:44 +0700

    change config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongo_uri = process.env.MONGO_URI;
+const mongo_uri =
+  "mongodb+srv://BaoDPG1:WeSpot2021@clusterinit.kgrtq.mongodb.net/WeSpot?retryWrites=true&w=majority";
 
 const connectDatabase = () => {
   // Connecting to the database
