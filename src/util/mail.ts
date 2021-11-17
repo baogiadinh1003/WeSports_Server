b4aa0ef81d17a8e1cdc9cb58d12665d9f256b152
baogiadinh1003 <baodpgpd03125@gmail.com>
2021-11-17 15:54:58 +0700

    update reset pass

--- src/util/mail.ts
+++ src/util/mail.ts
@@ -13,20 +13,21 @@ var smtpTransport = nodemailler.createTransport({
   },
 });
 
-export const sendMail = (req: express.Request, res: express.Response) => {
-  let link = "https://we-sports-sv.herokuapp.com/" + "verify?id=" + req.body._id;
+export const sendMail = (
+  req: express.Request,
+  res: express.Response,
+  data?: string
+) => {
+  let link =
+    "https://we-sports-sv.herokuapp.com/" + "verify?id=" + req.body._id;
   let html =
     "Hello,<br> Please Click on the link to verify your email.<br><a href=" +
     link +
     ">Click here to verify</a>";
   let subject = "Please confirm your Email account";
   if (req.body.reset !== null && req.body.reset !== undefined) {
-    link =
-      "https://we-sports-sv.herokuapp.com/" + "resetpass?id=" + req.body._id;
     html =
-      "Hello,<br> Please Click on the link to reset password your account.<br><a href=" +
-      link +
-      ">Click here to reset password</a>";
+      "Hello,<br> Here is your new password.<br>" + "<h2>" + data + "</h2>";
     subject = "Reset password WeSport account";
   }
 
@@ -68,33 +69,34 @@ export const verifyEmail = async (
   }
 };
 
-export const resetPass = async (req: express.Request, res: express.Response) => {
-  if (req.query.id === undefined) {
-    return false;
-  }
-  let rsRenter = await Renter.findById(req.query.id);
+export const resetPass = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  let rsRenter = await Renter.findOne({ renterEmail: req.body.to });
   if (rsRenter !== null) {
     return await resetPasswordRenter(req, res);
   }
-  let rsOwner = await Owner.findById(req.query.id);
+  let rsOwner = await Owner.findOne({ ownerEmail: req.body.to });
   if (rsOwner !== null) {
-    return await resetPasswordOwner(req,res);
+    return await resetPasswordOwner(req, res);
   }
-  return false; 
-}
+  return false;
+};
 
 const resetPasswordRenter = async (
   req: express.Request,
   res: express.Response
 ) => {
-  let accountId = req.query.id;
-  let account = await Renter.findById(accountId);
+  let account = await Renter.findOne({ renterEmail: req.body.to });
   if (account !== null) {
     try {
       if (account.accountStatus === 3) {
         return res.status(200).send("Account has been banned");
       }
-      await account.updateOne({ renterPassword: "12345678" });
+      let newPass = Math.round(Math.random() * 100000000);
+      await account.updateOne({ renterPassword: newPass.toString() });
+      await sendMail(req, res, newPass.toString());
       return res.status(200).send("0");
     } catch (error) {
       return res.status(500).send("Server error");
@@ -108,14 +110,15 @@ const resetPasswordOwner = async (
   req: express.Request,
   res: express.Response
 ) => {
-  let accountId = req.query.id;
-  let account = await Owner.findById(accountId);
+  let account = await Owner.findOne({ ownerEmail: req.body.to });
   if (account !== null) {
     try {
       if (account.accountStatus === 3) {
         return res.status(200).send("Account has been banned");
       }
-      await account.updateOne({ ownerPassword: "12345678" });
+      let newPass = Math.round(Math.random() * 100000000);
+      await account.updateOne({ ownerPassword: newPass.toString() });
+      await sendMail(req, res, newPass.toString());
       return res.status(200).send("0");
     } catch (error) {
       return res.status(500).send("Server error");
